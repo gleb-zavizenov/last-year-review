30668a6b1ab84447490f84d5a5ed5de728ffd268
Zavizenov <g_zavizenov@ats-hr420-dm14.local>
2019-09-19 13:13:52 -0400

    Finished second part

--- js/modules/DataObject.js
+++ js/modules/DataObject.js
@@ -3,28 +3,28 @@ export default {
     Trevor: {
         name: "Trevor",
         role: "professor",
-        bio: "Anime",
+        bio: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod dolores voluptate odio iusto vel aperiam expedita aut ullam maiores accusantium. Porro dolorem in consequatur assumenda quaerat distinctio esse, quisquam perspiciatis!",
         avatar: "olaf.jpeg"
     },
 
     Justin: {
         name: "Justin",
         role: "professor",
-        bio: "Anime",
+        bio: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod dolores voluptate odio iusto vel aperiam expedita aut ullam maiores accusantium. Porro dolorem in consequatur assumenda quaerat distinctio esse, quisquam perspiciatis!",
         avatar: "justin.jpeg"
     },
 
     Joe: {
         name: "Joe",
         role: "professor",
-        bio: "Anime",
+        bio: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod dolores voluptate odio iusto vel aperiam expedita aut ullam maiores accusantium. Porro dolorem in consequatur assumenda quaerat distinctio esse, quisquam perspiciatis!",
         avatar: "pooh.jpeg"
     },
 
     Jarrod: {
         name: "Jarrod",
         role: "professor",
-        bio: "Anime",
+        bio: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod dolores voluptate odio iusto vel aperiam expedita aut ullam maiores accusantium. Porro dolorem in consequatur assumenda quaerat distinctio esse, quisquam perspiciatis!",
         avatar: "zoolander.jpg"
     }
 }
